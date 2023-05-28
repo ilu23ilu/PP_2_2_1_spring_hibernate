70f92fd88b5728ba316ac1b13aa9eff823822744
Chudov Ilya <chudow-iluha@mail.ru>
2023-05-28 19:41:45 +0300

    first commit

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -1,5 +1,6 @@
 package hiber.dao;
 
+import hiber.model.Car;
 import hiber.model.User;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -26,4 +27,11 @@ public class UserDaoImp implements UserDao {
       return query.getResultList();
    }
 
+   @Override
+   public User getUser(Car car) {
+      return sessionFactory.getCurrentSession().createQuery("select u from User u join u.usersCar c where c.model = :model and c.series = :series", User.class)
+              .setParameter("model", car.getModel())
+              .setParameter("series", car.getSeries())
+              .uniqueResult();
+   }
 }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -18,6 +18,9 @@ public class User {
 
    @Column(name = "email")
    private String email;
+   @OneToOne (cascade = CascadeType.ALL)
+   @JoinColumn (name = "car_id")
+   private Car usersCar;
 
    public User() {}
    
@@ -25,6 +28,13 @@ public class User {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+   }
+
+   public User(String firstName, String lastName, String email, Car usersCar) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.usersCar = usersCar;
    }
 
    public Long getId() {
@@ -57,5 +67,24 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   public Car getUsersCar() {
+      return usersCar;
+   }
+
+   public void setUsersCar(Car usersCar) {
+      this.usersCar = usersCar;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", usersCar=" + usersCar +
+              '}';
    }
 }
